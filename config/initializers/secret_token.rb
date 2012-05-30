9f43bf63155ee0b533df4e4d0e86c2668c582297
RobinWu <robin.wu.cn@gmail.com>
2012-05-30 16:03:37 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Markitup::Application.config.secret_token = 'dbcb54490033741badfaf1de15b95cd21257f31a64b98f74d992ae57562838fbc9596c5d0e4b4f6c2c5084ce6da97d7c67a9a7ad754588061c824aed96ab9e26'
